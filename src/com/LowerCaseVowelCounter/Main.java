8ea5e25061aa67d713f864106fc88376c1d2e591
Campbell20 <jcampbell145@alamo.edu>
2017-04-01 17:03:53 -0500

    main program v.01

---
+++ src/com/LowerCaseVowelCounter/Main.java
@@ -0,0 +1,13 @@
+package com.LowerCaseVowelCounter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LowerCaseVowelCounter objLowerCase = new LowerCaseVowelCounter();
+        //continue to ask the user to create a string, and count the vowels of the string. No exit enabled
+        while (true){
+            objLowerCase.PrintVowelCounter();
+        }
+
+    }
+}
